21739f5670043cf7e716c33939c12ca2ed194964
ËnioDev <eniomejia57@gmail.com>
2023-08-23 11:42:24 -0600

    Promise.all

--- source/concepts/03-promises.js
+++ source/concepts/03-promises.js
@@ -29,18 +29,41 @@ export const promisesComponent = ( element ) => {
     const id1 = '5d86371f233c9f2425f16916';
     const id2 = '5d86371fd55e2e2a30fe1ccb';
 
-    findHero( id1 )
-        .then( (hero1) =>  {
 
-            findHero( id2 )
-                .then( hero2 => {
-                    renderTwoHeroes(hero1, hero2)
-                })
+    Promise.all([
+        findHero(id1),
+        findHero(id2),
+    ])
+    .then( ([hero1, hero2]) => renderTwoHeroes(hero1, hero2))
+    .catch( renderError );
 
-                .catch( renderError );
 
-        })
-        .catch( renderError );
+    // !Forma 2
+    // let hero1;
+
+    // findHero(id1)
+    //     .then( hero => {
+    //         hero1 = hero;
+    //         return findHero(id2);
+    //     }).then( hero2 => {
+    //         renderTwoHeroes( hero1, hero2);
+    //     })
+    //     .catch( renderError );
+
+
+    // !Forma 1
+    // findHero( id1 )
+    //     .then( (hero1) =>  {
+
+    //         findHero( id2 )
+    //             .then( hero2 => {
+    //                 renderTwoHeroes(hero1, hero2)
+    //             })
+
+    //             .catch( renderError );
+
+    //     })
+    //     .catch( renderError );
 
 }
 
