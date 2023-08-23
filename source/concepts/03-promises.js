9b67692f409dec6610b4f9becf7614b72c70c801
ËnioDev <eniomejia57@gmail.com>
2023-08-23 11:06:40 -0600

    Como se genera una Promise Hell

--- source/concepts/03-promises.js
+++ source/concepts/03-promises.js
@@ -10,6 +10,15 @@ export const promisesComponent = ( element ) => {
         element.innerHTML = hero.name;
     }
 
+    const renderTwoHeroes = ( hero1, hero2 ) => {
+        element.innerHTML = `
+
+            <h3>${ hero1.name }</h3>
+            <h3>${ hero2.name }</h3>
+
+        `;
+    }
+
     const renderError = ( error ) => {
         element.innerHTML = `
             <h1>Error:</h1>
@@ -18,9 +27,19 @@ export const promisesComponent = ( element ) => {
     }
 
     const id1 = '5d86371f233c9f2425f16916';
+    const id2 = '5d86371fd55e2e2a30fe1ccb';
 
     findHero( id1 )
-        .then( renderHero )
+        .then( (hero1) =>  {
+
+            findHero( id2 )
+                .then( hero2 => {
+                    renderTwoHeroes(hero1, hero2)
+                })
+
+                .catch( renderError );
+
+        })
         .catch( renderError );
 
 }
