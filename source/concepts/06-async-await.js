4ac363d13c544c0a9721ba9457e13e3c82cc66d7
ËnioDev <eniomejia57@gmail.com>
2023-08-25 17:13:03 -0600

    Manejo de errores en Async-Await

--- source/concepts/06-async-await.js
+++ source/concepts/06-async-await.js
@@ -9,11 +9,16 @@ export const asynAwaitComponent = async( element ) => {
     const id1 = '5d86371fd55e2e2a30fe1ccb2';
     const id2 = '5d86371fd55e2e2a30fe1cc3';
 
-    const hero1 = await findHero( id1 ); //! DESESTRUCTURAR?
-    const hero2 = await findHero( id2 );
+    try {
+        const hero1 = await findHero( id1 ); //! DESESTRUCTURAR?
+        const hero2 = await findHero( id2 );
+        
+        element.innerHTML = `${ hero1.name } / ${ hero2.name }`;
 
+    } catch (error) {
+        element.innerHTML = error;
+    }
 
-    element.innerHTML = `${ hero1.name } / ${ hero2.name }`;
 
 }
 
